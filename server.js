dd256c8b61c60d0697dd55cc83093acca677ed1c
Krishna <bskrishna@gmail.com>
2017-05-17 05:20:01 +0000

    Final version

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:dateStr', function (req, res) {
+    var date = req.query;
+    var natStrDate;
+    var numDate;
+    var resObj = 
+    {
+        unix: "",
+        natural: ""
+    };
+
+    res.writeHead(200, {'content-type':'text/html'});
+
+    natStrDate = new Date(req.params.dateStr);
+    numDate = new Date(Number(req.params.dateStr));
+
+    if(isFinite(natStrDate)) {
+        resObj.natural = req.params.dateStr;
+        resObj.unix = Date.parse(natStrDate);
+    } else if(isFinite(numDate)) {
+        //resObj.natural = numDate.getMonth() + " " + 
+            //numDate.getDate() + ", " + numDate.getYear();
+        resObj.natural = numDate.toDateString();
+        resObj.unix = Date.parse(numDate);
+    }
+    res.write(JSON.stringify(resObj));
+    res.end();
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
